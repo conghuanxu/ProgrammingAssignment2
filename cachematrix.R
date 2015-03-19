f5a5f0ace2f43983c285288c7650fc2dfbb15949
conghuanxu <conghuaxxx@gmail.com>
2015-03-18 20:43:28 -0700

    homework

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,11 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## This function have two goals, one is give the inverse of a matrix, 
+## the other goal is if we have computed the inverse once, and if we 
+## need compute the inverse again we can read from the cache
 
 ## Write a short comment describing this function
-
+## This function is to make the cache of the inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
         set <- function(y) {
@@ -19,7 +22,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+## if the cached data is null, we solve the inverse and make cache
+## otherwise we read data from the cached data.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inv <- x$getinv()
